bb32d856caa3003dc208777095d1a9bd6a2728e3
chadNoliver <chadNoliver@gmail.com>
2022-02-15 21:19:38 -0600

    add incidents per m^2

--- analysis/access-census-api.R
+++ analysis/access-census-api.R
@@ -5,7 +5,7 @@ options(tigris_use_cache = TRUE)
 key <- read.delim(file="private/Census Api Key.txt",header=FALSE)
 Sys.setenv(census_api_key=key[[1]])
 
-census <- st_as_sf(tidycensus::get_estimates(year=2019, geography="county", geometry=TRUE, product="population")) %>%
+census_split <- st_as_sf(tidycensus::get_estimates(year=2019, geography="county", geometry=TRUE, product="population")) %>%
   filter(variable=="POP") %>%
   st_transform(2163)
 
